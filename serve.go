be713e5eebe19727cc8f7e50dcb638061e2fc960
susabi <xiaodaigh@users.noreply.github.com>
2017-07-07 21:15:13 +1000

    minor go server

--- serve.go
+++ serve.go
@@ -1,18 +1,8 @@
 package main
 
-import (
-	// "fmt"
-	"net/http"
-)
+import "net/http"
 
 func main() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Printf(r.URL.Path[0:] + "\n")
-	// 	http.ServeFile(w, r, r.URL.Path[0:])
-
-	// })
-
 	http.Handle("/", http.FileServer(http.Dir("")))
-
 	http.ListenAndServe(":8080", nil)
 }
