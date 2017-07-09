a25f937ca2a6f119712eb438cc7bc6a816c46606
susabi <xiaodaigh@users.noreply.github.com>
2017-07-09 19:52:44 +1000

    localised react and made frequency reactive

--- js/tax_calc.jsx
+++ js/tax_calc.jsx
@@ -31,7 +31,7 @@ function incomeTax(income, incomeBracket = [18200, 37000, 87000, 180000, Infinit
 
 // incomeBracket = [18200, 37000, 87000, 180000, Infinity]
 // income = 180000
-//incomeTax(30000, incomeBracket, [0, 0, 3572, 19822, 54232], [0, 0.19, 0.325, 0.37, 0.45])
+// incomeTax(30000, incomeBracket, [0, 0, 3572, 19822, 54232], [0, 0.19, 0.325, 0.37, 0.45])
 
 // calculate salary given tax
 function taxToSalary(tax, incomeBracket = [18200, 37000, 87000, 180000, Infinity], 
@@ -83,83 +83,155 @@ function postTaxToSalary(postTax, incomeBracket = [18200, 37000, 87000, 180000, 
 
 	///if(i == -1) i = incomeBracket.length-1
 
-
 	var incomeThreshold =  i == 0 ? 0 : incomeBracket[i-1]
 		
 	//console.log([baseTax[i], incomeThreshold,marginalRate[i]])
 
 	return (postTax + baseTax[i] - incomeThreshold*marginalRate[i])/(1-marginalRate[i])
-
 }
 
-// taxToSalary(20000, 
-// 	incomeBracket, [0, 0, 3572, 19822, 54232],
-// 	 [0, 0.19, 0.325, 0.37, 0.45])
+
+function unAnnualiseSalaryFactor(frequency) {
+	var mult
+	switch (frequency) {
+  		case "Monthly" : mult =1/12; break;
+  		case "Fornightly" : mult = 1/26;  break;
+  		case "Weekly" : mult = 1/50;  break;
+  		case "Daily" : mult = 1/250;  break;
+  		default: mult = 1
+  	}
+  	return mult;
+}
+
+function annualiseSalaryFactor(frequency) {
+	var mult
+	switch (frequency) {
+  		case "Monthly" : mult =12; break;
+  		case "Fornightly" : mult = 26;  break;
+  		case "Weekly" : mult = 52;  break;
+  		case "Day Rate" : mult = 250;  break;
+  		default: mult = 1
+  	}
+  	return mult;
+}	
+
+function annualiseSalary(salary, frequency) {
+	return salary * annualiseSalaryFactor(frequency)
+}
+
+function unAnnualiseSalary(annualSalary, frequency) {
+  	return annualSalary / annualiseSalaryFactor(frequency);
+}
 
 //React JS calculator class
 class TaxCalculator extends React.Component {
   constructor(props) {
     super(props);
-    this.handleSalaryChange = this.handleSalaryChange.bind(this);
-    this.handleTaxChange = this.handleTaxChange.bind(this)
+    this.handleSalaryInputChange = this.handleSalaryInputChange.bind(this);
+    this.handleTaxInputChange = this.handleTaxInputChange.bind(this)
     this.handlePostTaxChange = this.handlePostTaxChange.bind(this)
-
+    this.handleFreqChange = this.handleFreqChange.bind(this)
+    
+    var newTax = incomeTax(props.salary)
     this.state = {
+    	frequency:"Annual",
+    	salaryInput:props.salary,
     	salary: props.salary, 
-    	tax : incomeTax(props.salary), 
+    	tax : newTax, 
+    	taxInput:newTax,
     	medicareLevy:props.salary*0.025, 
     	superannuation : props.salary * props.super_pct/100, 
     	super_pct : props.super_pct, 
-    	postTax : props.salary  - incomeTax(props.salary) - props.salary*0.025
+    	postTax : props.salary  - incomeTax(props.salary) - props.salary*0.025,
+    	postTaxInput:props.salary  - incomeTax(props.salary) - props.salary*0.025
     	};
   }
 
-  handleSalaryChange(e) {
-  	var etv = eval(e.target.value)
-    if (isFinite(etv)) {
-    	var new_tax = incomeTax(etv)
-    	var new_post_tax = etv - new_tax
-    	var new_medicareLevy = etv * 0.025
+  handleFreqChange(e) {
+  	var newFrequency = e.target.value
+
+  	var newSalaryInput = unAnnualiseSalary(this.state.salary, newFrequency)
+  	var newTaxInput = Math.round(unAnnualiseSalary(incomeTax(this.state.salary), newFrequency))
+  	var newMedicareLevy = newSalaryInput * 0.025
+  	var newPostTaxInput = newSalaryInput - newTaxInput - newMedicareLevy
+
+  	
+  	this.setState({
+  		salaryInput: Math.round(newSalaryInput),
+  		taxInput: Math.round(newTaxInput), 
+  		postTaxInput : Math.round(newPostTaxInput),
+  		frequency : newFrequency
+  	})
+  }
+
+  handleSalaryInputChange(e) {
+  	var newSalaryInput = eval(e.target.value)
+
+    if (isFinite(newSalaryInput)) {
+    	var newSalary = annualiseSalary(newSalaryInput, this.state.frequency)
+    	var newTax = incomeTax(newSalary)
+    	var newMedicareLevy = newSalary * 0.025	
+    	var newPostTax = newSalary - newTax - newMedicareLevy
+
+	  	var newTaxInput = Math.round(unAnnualiseSalary(newTax, frequency))
+	  	var newMedicareLevy = newSalaryInput * 0.025
+	  	var newPostTaxInput = newSalaryInput - newTaxInput - newMedicareLevy
+	    	
     	this.setState({
-	    	salary : e.target.value, 
-	    	tax: Math.round(new_tax), 
-	    	postTax: Math.round(new_post_tax - new_medicareLevy),
-	    	medicareLevy : new_medicareLevy,
-	    	superannuation : etv * this.state.super_pct/100
+	    	salary : Math.round(newSalary),
+	    	salaryInput: e.target.value,
+	    	tax: Math.round(newTax), 
+	    	postTax: Math.round(newPostTax - newMedicareLevy),
+	    	medicareLevy : Math.round(newMedicareLevy),
+	    	superannuation : Math.round(newSalary * this.state.super_pct/100),
+	    	taxInput:newTaxInput,
+	    	postTaxInput:newPostTax
     	});
     } else {
     	this.setState({
-    		salary: e.target.value, 
-    		super_pct : this.state.super_pct
+    		salaryInput : e.target.value
     	})
     }
   }
 
-  handleTaxChange(e) {
-  	var etv = eval(e.target.value)
-  	if (isFinite(etv)) {
-  		var new_sal = taxToSalary(etv)
-    	var new_post_tax = new_sal - etv
-    	var new_medicareLevy = new_sal * 0.025
-  		this.setState({salary : Math.round(new_sal), tax: e.target.value, 
-  			postTax : Math.round(new_post_tax),
-  			medicareLevy : new_medicareLevy,
-  			superannuation : new_sal * this.state.super_pct/100});
+  handleTaxInputChange(e) {
+  	var newTaxInput = eval(e.target.value)
+  
+  	if (isFinite(newTaxInput)) {
+  		var newTax = annualiseSalary(newTaxInput, this.state.newTaxInput)
+  		var newSal = taxToSalary(newTax)
+    	var newMedicareLevy = newSal * 0.025
+    	var newPostTax = newSal - newTax - newMedicareLevy
+  		this.setState({
+  			salary : Math.round(newSal), 
+  			salaryInput : Math.round(newSal), 
+  			tax : Math.round(newTax),
+  			taxInput: e.target.value, 
+  			postTax : Math.round(newPostTax),
+  			medicareLevy : Math.round(newMedicareLevy),
+  			superannuation : Math.round(newSal * this.state.super_pct/100)});
   	} else {
-  		this.setState({tax: e.target.value});
+  		this.setState({taxInput: e.target.value});
   	}
   }
 
   handlePostTaxChange(e) {
-  	var etv = eval(e.target.value)
-  	if (isFinite(etv)) {
-  		var new_sal = postTaxToSalary(etv);
-  		var new_tax = incomeTax(new_sal)
-		var new_medicareLevy = new_sal * 0.025	
-  		this.setState({salary : Math.round(new_sal), tax: Math.round(new_tax), 
+  	var newPostTaxInput = eval(e.target.value)
+
+  	if (isFinite(newPostTaxInput)) {
+  		var newPostTax = annualiseSalary(newPostTaxInput, this.state.frequency)
+  		var newSalary = postTaxToSalary(newPostTax);
+  		var newTax = incomeTax(newSalary)
+		var newMedicareLevy = newSalary * 0.025	
+  		this.setState({
+  			salary : Math.round(newSalary),
+  			tax: Math.round(newTax), 
   			postTax:e.target.value, 
-  			medicareLevy : new_medicareLevy,
-  			superannuation : new_sal * this.state.super_pct/100
+  			medicareLevy : newMedicareLevy,
+  			superannuation : newSalary * this.state.super_pct/100,
+  			salaryInput : unAnnualiseSalary(newSalary, this.state.frequency),
+  			taxInput : unAnnualiseSalary(newTax, this.state.frequency),
+  			postTaxInput : e.target.value
   		});
   	} else {
   		this.setState({postTax: e.target.value});
@@ -173,14 +245,16 @@ class TaxCalculator extends React.Component {
     const superannuation = this.state.superannuation;
     const super_pct = this.state.super_pct;
     const postTax = this.state.postTax;
-
+    const salaryInput = this.state.salaryInput
+    const taxInput = this.state.taxInput
+    const postTaxInput = this.state.postTaxInput
 
     return (
      <form> 
      	<div className="row">
 			<div className="two columns">
-				<label for="frequency">Frequency</label>
-				<select className = "u-full-width" id="frequency">
+				<label htmlFor="frequency">Frequency</label>
+				<select className = "u-full-width" id="frequency" onChange = {this.handleFreqChange}>
 					<option value="Annual">Annual</option>
 					<option value="Monthly">Monthly</option>
 					<option value="Fornightly">Fornightly</option>
@@ -190,16 +264,16 @@ class TaxCalculator extends React.Component {
 			</div>
 			<div className="two columns">
 				<label htmlFor="salaryInput">Salary ${Math.round(eval(salary)).toLocaleString()} </label>
-				<input className = "u-full-width" placeholder={Math.round(salary)} value = {salary} id="salaryInput" onChange = {this.handleSalaryChange}/>
+				<input className = "u-full-width" placeholder={Math.round(salaryInput)} value = {salaryInput} id="salaryInput" onChange = {this.handleSalaryInputChange}/>
 			</div>
 	
 	      	<div className="two columns">
 	      		<label htmlFor="taxInput">Tax ${Math.round(eval(tax)).toLocaleString()} </label>
-				<input className = "u-full-width" placeholder={Math.round(tax)} value = {tax} id="taxInput" onChange = {this.handleTaxChange}/>
+				<input className = "u-full-width" placeholder={Math.round(tax)} value = {taxInput} id="taxInput" onChange = {this.handleTaxInputChange}/>
 			</div>
 			<div className="two columns">
 	      		<label htmlFor="afterTaxInput">Post tax ${Math.round(eval(postTax)).toLocaleString()} </label>
-				<input className = "u-full-width" placeholder={Math.round(postTax)} value = {postTax} id="afterTaxInput" onChange = {this.handlePostTaxChange}/>
+				<input className = "u-full-width" value = {postTaxInput} id="afterTaxInput" onChange = {this.handlePostTaxChange}/>
 	      	</div>
 	      	<div className="two columns">
 	      		<label htmlFor="medicareLevy_show">Medicare Levy </label>
@@ -220,15 +294,15 @@ class TaxCalculator extends React.Component {
 
 ReactDOM.render(
 	<div>
-	<div class = "row">
+	<div className = "row">
 		<h3> Calculation 1</h3>
 	</div>
-	<TaxCalculator salary = "80000" super_pct = "9.5"/> 
+	<TaxCalculator salary = "78000" super_pct = "9.5"/> 
 
-	<div class = "row">
+	<div className = "row">
 		<h3> Calculation 2</h3>
 	</div>
-	<TaxCalculator  salary = "70000" super_pct = "9.5"/>
+	<TaxCalculator  salary = "65000" super_pct = "9.5"/>
 	</div>,
 		document.getElementById('root')
 );
