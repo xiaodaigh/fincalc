2740080f982ba43996e2a4b3772d82463025118b
susabi <zhuojia.dai@gmail.com>
2017-09-11 21:20:23 +1000

    move script to end of body

--- dist/sw.js
+++ dist/sw.js
@@ -3,7 +3,7 @@
 //sw.js
 
 var CACHE_NAME = 'fincalc-cache-v1';
-var urlsToCache = ['/', '/Skeleton-2.0.4/css/normalize.css', '/Skeleton-2.0.4/css/skeleton.css', '/css/custom.css', '/css/googleapifont.css', '/images/favicon-192x192.png', '/dist/site.js', "/images/apple-touch-icon-57x57.png", "/images/apple-touch-icon-60x60.png", "/images/apple-touch-icon-72x72.png", "/images/apple-touch-icon-114x114.png", "/images/apple-touch-icon-144x144.png", "/dist/tax_calc.js", "/dist/react.js", "/dist/react-dom.js", "/dist/jquery.min.js"];
+var urlsToCache = ['/', '/sw.js', '/Skeleton-2.0.4/css/normalize.css', '/Skeleton-2.0.4/css/skeleton.css', '/css/custom.css', '/css/googleapifont.css', '/images/favicon-192x192.png', "/dist/jquery.min.js", '/dist/site.js', "/images/apple-touch-icon-57x57.png", "/images/apple-touch-icon-60x60.png", "/images/apple-touch-icon-72x72.png", "/images/apple-touch-icon-114x114.png", "/images/apple-touch-icon-144x144.png", "/dist/tax_calc.js", "/dist/react.js", "/dist/react-dom.js", "/js/site.js"];
 
 self.addEventListener('install', function (event) {
   // Perform install steps
