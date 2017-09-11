41f92bfecce368c35f9ae6fcef525fb43bee6f73
susabi <zhuojia.dai@gmail.com>
2017-09-11 20:08:45 +1000

    more icons for pwa and sw

---
+++ dist/sw.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//sw.js
+
+var CACHE_NAME = 'fincalc-cache-v1';
+var urlsToCache = ['/', '/Skeleton-2.0.4/css/normalize.css', '/Skeleton-2.0.4/css/skeleton.css', '/css/custom.css', '/images/favicon-192x192.icon', '/dist/site.js', "/images/apple-touch-icon-57x57.png", "/images/apple-touch-icon-60x60.png", "/images/apple-touch-icon-72x72.png", "/images/apple-touch-icon-114x114.png", "/images/apple-touch-icon-144x144.png", "/dist/tax_calc.js"];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+    // console.log('Opened cache');
+    return cache.addAll(urlsToCache);
+  }));
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(function (response) {
+    // Cache hit - return response
+    if (response) {
+      return response;
+    }
+    return fetch(event.request);
+  }));
+});
